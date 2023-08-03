61e355c8772732c0398d4390738f3f8dc53df0ec
chaks <manochitra2003@gmail.com>
2023-08-03 14:00:49 +0530

    new

--- test.js
+++ test.js
@@ -4,5 +4,57 @@ console.log("the ans is",a)
     a=26
     console.log("the ans aft update",a)
 }
+
+let a=20
+
+console.log("the ans is",a)
+{
+    let a=26
+    console.log("the ans aft update",a)
+}
+let b=50
+console.log("the ans is",b)
+const c=20
+console.log("the ans is",c)
+const c=50
+console.log("the ans is",c)
+
 a=26
-    console.log("the ans aft update out",a)
+if(a>0){
+   console.log("positive")
+}
+else if(a==0){
+    console.log("zero!!")
+ }
+else {
+    console.log("negative")
+ }
+
+ let a=0
+ if(a>0){
+    console.log("positive")
+ }
+ else if(a==0){
+     console.log("zero!!")
+     a=a+1
+  }
+console.log(a)
+ else {
+     console.log("negative")
+  }
+
+FOR
+a=5
+for (i=0; i<a;i++){
+    console.log(i)
+}
+
+// WHILE
+i=0
+while(i<=a){
+    console.log(i)
+    i++
+}
+
+array = [10,20]
+array.forEach(element => console.log(element));
