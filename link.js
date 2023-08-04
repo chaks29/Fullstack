5190f7cd67672e9f858e7faa352036a38c59433a
chaks <manochitra2003@gmail.com>
2023-08-04 09:41:56 +0530

    new

---
+++ link.js
@@ -0,0 +1,14 @@
+function first(){
+    document.getElementById("hello").innerHTML= "I am in First Year"
+}
+function second(){
+    document.getElementById("hello").innerHTML= "I am in Second Year"
+}
+
+function third(){
+    document.getElementById("hello").innerHTML= "I am in Third Year"
+}
+
+function fourth(){
+    document.getElementById("hello").innerHTML= "I am in Fourth Year"
+}
